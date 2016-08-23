dafcbdd02f023f34b8fefede44be221c32c9234b
Ayush Kumar <ayush_94@hotmail.com>
2016-08-23 13:44:34 +1000

    Added department and branch entity to models

---
+++ IdentityRight/src/IdentityRight/Models/Organisation/Branches.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityRight.Models
+{
+    public class Branches
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }  
+           
+        public ApplicationOrganisations ApplicationOrganisations { get; set; }
+        public int ApplicationOrganisationsId { get; set; }
+
+        public Locations Locations { get; set; }
+        public int LocationsId { get; set; }
+    }
+}
---
+++ IdentityRight/src/IdentityRight/Services/LinkRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityRight.Models;
+
+namespace IdentityRight.Services
+{
+    public class LinkRepository
+    {
+        public bool DoesLinkExist()
+        {
+            return true;
+        }
+    }
+}
