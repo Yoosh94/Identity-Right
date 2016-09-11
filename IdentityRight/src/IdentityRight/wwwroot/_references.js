3131c1514bcb253a69741ceb06a4b4243db62136
felix <felix@2mar.com>
2016-09-11 15:09:21 +1000

    Made text black for buttons.

--- IdentityRight/src/IdentityRight/wwwroot/_references.js
+++ IdentityRight/src/IdentityRight/wwwroot/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/jquery.js" />
 /// <reference path="js/linkorgs.js" />
 /// <reference path="js/mapsapi.js" />
 /// <reference path="js/site.js" />
