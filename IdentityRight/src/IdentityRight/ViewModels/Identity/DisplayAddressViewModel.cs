0d71bd0aa4191b8cc018eccb8fe9817f3a46ce54
Ayush Kumar <ayush_94@hotmail.com>
2016-09-11 22:44:58 +1000

    User is able to see all their registered addresses

---
+++ IdentityRight/src/IdentityRight/ViewModels/Identity/DisplayAddressViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityRight.Models;
+using IdentityRight.Services;
+
+namespace IdentityRight.ViewModels.Identity
+{
+    public class DisplayAddressViewModel
+    {
+        public List<UserAddresses> userAddress { get; }
+        public List<Locations> location { get; }
+
+        public DisplayAddressViewModel()
+        {
+            userAddress = new List<UserAddresses>();
+            location = new List<Locations>();
+        }
+    }
+}
