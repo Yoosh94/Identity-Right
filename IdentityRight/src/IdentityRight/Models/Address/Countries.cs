7ba11cc3551a2c10d459987d2783cdc00159af73
Ayush Kumar <ayush_94@hotmail.com>
2016-09-16 20:39:59 +1000

    Added display name to location model properties

--- IdentityRight/src/IdentityRight/Models/Address/Countries.cs
+++ IdentityRight/src/IdentityRight/Models/Address/Countries.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IdentityRight.Models
 {
@@ -6,9 +7,11 @@ namespace IdentityRight.Models
     {
         public int Id { get; set; }
         public List<Locations> Locations { get; set; }
+        [Display(Name = "Country")]
         public string countryName { get; set; }
         //Foreign Keys
         public Regions Region { get; set; }
+        [Display(Name = "Region")]
         public int RegionsId { get; set; }
     }
 }
