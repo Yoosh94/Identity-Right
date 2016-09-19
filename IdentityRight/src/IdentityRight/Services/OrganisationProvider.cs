14ebd470acd4db707c07cfcab2f9d74daaf592e5
ahmedshash <ahmed.shash29@gmail.com>
2016-09-19 19:39:51 +1000

    Added: Comments on MessageServices & OrganisationProvider

--- IdentityRight/src/IdentityRight/Services/OrganisationProvider.cs
+++ IdentityRight/src/IdentityRight/Services/OrganisationProvider.cs
@@ -17,16 +17,27 @@ namespace IdentityRight.Services
     {
         private readonly ApplicationDbContext _dbContext;
 
+        // Create instance of the organisation provider
         public OrganisationProvider()
         {
             _dbContext = new ApplicationDbContext();
         }
 
+        /// <summary>
+        /// Gets all of the users that are currently linked with an organisation
+        /// </summary>
+        /// <param name="organisationId"></param>
+        /// <returns>A list of users</returns>
         public List<ApplicationUser> GetAllUsersInOrganisation(int organisationId)
         {
             return _dbContext.UserOrganisationLinks.Where(a => a.ApplicationOrganisationsId == organisationId).Select(l => l.ApplicationUser).ToList();
         }
 
+        /// <summary>
+        /// Gets the details of a particular organisation, including the list of linked users
+        /// </summary>
+        /// <param name="organisationId"></param>
+        /// <returns>The organisationdetailsviewmodel containing Organisation Name, Address and List of linked users</returns>
         public OrganisationDetailsViewModel GetOrganisationDetails(int organisationId)
         {
             var org = _dbContext.ApplicationOrganisations.FirstOrDefault(z => z.Id == organisationId);
@@ -40,6 +51,10 @@ namespace IdentityRight.Services
             return model;
         }
 
+        /// <summary>
+        /// Gets a list of all the organisations
+        /// </summary>
+        /// <returns>The organisationviewmodel containing a list of organisations</returns>
         public OrganisationsViewModel GetAllOrganisations()
         {
             return new OrganisationsViewModel
