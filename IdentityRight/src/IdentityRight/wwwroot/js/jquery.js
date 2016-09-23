c3d8080ada890cccdde70a59ddb08650983f7f63
Mark.Serdina <mans.mserdi@gmail.com>
2016-09-23 23:50:13 +1000

    home thumbnail edits

--- IdentityRight/src/IdentityRight/wwwroot/js/jquery.js
+++ IdentityRight/src/IdentityRight/wwwroot/js/jquery.js
@@ -1,5 +1,8 @@
-﻿$('.email-btn').on('click', function () {
-    $parent_box = $(this).closest('#btn-option');
-    $parent_box.siblings().find('#email-update').hide();
-    $parent_box.find('#email-form').toggle();
-});
+﻿ $('.email-btn').on('click', function () {
+        $parent_box = $(this).closest('#btn-option');
+        $parent_box.siblings().find('#email-update').hide();
+        $parent_box.find('#email-form').toggle();
+    });
+
+
+ 
