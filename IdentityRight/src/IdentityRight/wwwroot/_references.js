d981c28bd0c59446c2439c230ef6cf5e9a85e937
felix <felix@2mar.com>
2016-09-24 14:15:34 +1000

    Improved look of phone add/update page

--- IdentityRight/src/IdentityRight/wwwroot/_references.js
+++ IdentityRight/src/IdentityRight/wwwroot/_references.js
@@ -4,6 +4,7 @@
 /// <reference path="js/linkorgs.js" />
 /// <reference path="js/mapsAPI.js" />
 /// <reference path="js/site.js" />
+/// <reference path="js/updatenumbers.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
---
+++ IdentityRight/src/IdentityRight/wwwroot/js/updateNumbers.js
@@ -0,0 +1,20 @@
+﻿function collapseAll() {
+    $("#mobiles").collapse("hide");
+    $("#work").collapse("hide");
+    $("#home").collapse("hide");
+    $("#business").collapse("hide");
+
+    $("#c-e").html('<a href="#" onclick="expandAll();">Expand All ( + )</a>');
+
+
+}
+
+function expandAll() {
+    $("#mobiles").collapse("show");
+    $("#work").collapse("show");
+    $("#home").collapse("show");
+    $("#business").collapse("show");
+
+    $("#c-e").html('<a href="#" onclick="collapseAll();">Collapse All ( - )</a>');
+
+}
